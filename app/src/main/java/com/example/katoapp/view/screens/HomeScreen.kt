77e6a8f9e50b036b629a33e08216465ce4cca209
Zein Nur Ichsan <zeinnur@students.amikom.ac.id>
2025-12-03 14:39:27 +0700

    add padding homescreen

--- app/src/main/java/com/example/katoapp/view/screens/HomeScreen.kt
+++ app/src/main/java/com/example/katoapp/view/screens/HomeScreen.kt
@@ -3,17 +3,20 @@ package com.example.katoapp.view.screens
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeScreen(modifier: Modifier) {
     Column(modifier
         .fillMaxSize()
+        .padding(top = 48.dp)
         .background(MaterialTheme.colorScheme.background))
     {
         Text(
