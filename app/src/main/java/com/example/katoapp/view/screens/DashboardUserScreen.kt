41bf50949bb6e621cdab32141dc3967980e4e538
Zein Nur Ichsan <zeinnur@students.amikom.ac.id>
2025-12-10 15:00:35 +0700

    updateProfile

--- app/src/main/java/com/example/katoapp/view/screens/DashboardUserScreen.kt
+++ app/src/main/java/com/example/katoapp/view/screens/DashboardUserScreen.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -30,18 +29,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.FontScaling
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import com.example.katoapp.R
 import com.example.katoapp.view.component.Carousel
 import com.example.katoapp.view.component.SearchBar
-import com.example.katoapp.viewModel.AuthViewModel
 import com.example.katoapp.viewModel.DashboardUserViewModel
 
 @Composable
